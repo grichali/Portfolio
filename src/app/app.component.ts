06d33b639d3eb05380fe9c08b1765b818f2f2ac0
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-07-15 01:01:27 +0100

    adding images and skills component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HeroComponent,AboutComponent],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.css'
+})
+export class SkillsComponent {
+  skills = [
+    { imgPath: 'images/html.png', name: 'Html' },
+    { imgPath: 'images/css.png', name: 'Css' },
+    { imgPath: 'images/bootstrap.png', name: 'Bootstrap' },
+    { imgPath: 'images/net.png', name: '.Net' },
+    { imgPath: 'images/angular.png', name: 'Angular' },
+    { imgPath: 'images/laravel.png', name: 'Laravel' },
+    { imgPath: 'images/nestjs.png', name: '.NestJS' },
+    { imgPath: 'images/python.png', name: 'Python' },
+    { imgPath: 'images/mysql.png', name: 'MySQL' },
+  ];
+}
