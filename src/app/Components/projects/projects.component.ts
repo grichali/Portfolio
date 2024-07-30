55fa97042ab3926e24bf1a949c85e43e8d3c5a7e
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-07-30 22:02:00 +0100

    adjustion projects'section content

--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -12,33 +12,32 @@ export class ProjectsComponent {
 
   projects = [
     {
-      title: 'Consultation de destinations de voyage',
-      description: 'Ce site web offre une plateforme conviviale pour la consultation de destinations de voyage. Les utilisateurs peuvent explorer une variété de destinations et consulterdes avis d\'autres voyageurs.',
+      title: 'Travel Destination Consultation',
+      description: 'This website offers a user-friendly platform for consulting travel destinations. Users can explore a variety of destinations and read reviews from other travelers.',
       techStack: 'HTML/CSS, JavaScript, PHP.',
       codeLink: 'https://github.com/grichali/Voyage',
-      image:'images/Travel.jpg'
+      image: 'images/Travel.jpg'
     },
     {
-      title: 'Informatisation des Réservations de restaurants',
-      description:'Ce système permet aux restaurants de gérer leurs réservations en ligne. Il permet aux clients de réserver une table en ligne, et de consulter les menus. Le système permet également aux restaurants de gérer leurs tables',
+      title: 'Restaurant Reservation System',
+      description: 'This system allows restaurants to manage their reservations online. It enables customers to book a table online and view menus. The system also allows restaurants to manage their tables.',
       techStack: 'Laravel, MySql, Postman.',
-      image:'images/Restaurant.jpg',
-      codeLink: 'https://github.com/grichali/Restauration'
+      codeLink: 'https://github.com/grichali/Restauration',
+      image: 'images/Restaurant.jpg'
     },
     {
-      title: 'ResAPI pour la gestion des centres de soutien',
-      description:
-      'Ce projet permet aux étudiants de réserver des séances de tutorat et de consulter les avis sur les centres et les professeurs. De plus, les professeurs peuvent réserver des salles dans les centres, tandis que ces derniers peuvent gérer ces réservations et leurs salles correspondantes.',
+      title: 'API for Support Centers Management',
+      description: 'This project allows students to book tutoring sessions and read reviews about centers and teachers. Additionally, teachers can book rooms in the centers, while the centers can manage these reservations and their corresponding rooms.',
       techStack: 'NestJs, MySql, Angular.',
-      image:'images/Soutien.jpg',
-      codeLink: 'https://github.com/grichali/Centre'
+      codeLink: 'https://github.com/grichali/Centre',
+      image: 'images/Soutien.jpg'
     },
     {
-      title: 'Système de conduite autonome',
-      description: 'Ce projet consiste à développer un système utilisant des réseaux de neurones convolutifs (CNN) pour une voiture autonome. Ce système a permis la prise de décision et la navigation en toute sécurité.',
+      title: 'Autonomous Driving System',
+      description: 'This project involves developing a system using Convolutional Neural Networks (CNN) for an autonomous car. This system enabled safe decision-making and navigation.',
       techStack: 'Python, OpenCv, Pandas, SKlearn, Flask.',
-      image:'images/SelfDriving.jpg',
-      codeLink: 'https://github.com/grichali/Self-Driving-Car'
-    },
-  ]
+      codeLink: 'https://github.com/grichali/Self-Driving-Car',
+      image: 'images/SelfDriving.jpg'
+    }
+  ];
 }
